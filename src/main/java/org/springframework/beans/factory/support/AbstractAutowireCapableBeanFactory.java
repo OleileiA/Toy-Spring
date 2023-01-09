7a9d3f39356e4258369c6e7ccd14272d6c14b435
cyw <18604469853@163.com>
2023-01-09 17:17:00 +0800

    instantiation-strategy

--- src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -4,6 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
+
+	private InstantiationStrategy instantiationStrategy = new SimpleInstantiationStrategy();
+
 	@Override
 	protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
 		return doCreateBean(beanName, beanDefinition);
@@ -13,12 +16,24 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
 		Class beanClass = beanDefinition.getBeanClass();
 		Object bean = null;
 		try {
-			bean = beanClass.newInstance();
+			bean = createBeanInstance(beanDefinition);
 		} catch (Exception e) {
 			throw new BeansException("Instantiation of bean failed", e);
 		}
 
 		addSingleton(beanName, bean);
 		return bean;
+	}
+
+	protected Object createBeanInstance(BeanDefinition beanDefinition) {
+		return getInstantiationStrategy().instantiate(beanDefinition);
+	}
+
+	public InstantiationStrategy getInstantiationStrategy() {
+		return instantiationStrategy;
+	}
+
+	public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+		this.instantiationStrategy = instantiationStrategy;
 	}
 }
